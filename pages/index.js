76d4f9978c49a77671a0e90dd823c35cf7d07b05
Simba <joinsimba@gmail.com>
2021-10-03 18:29:41 +0200

    add feature section

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Navbar from '../components/landing-page/Navbar'
 import Hero from '../components/landing-page/Hero'
+import FeaturesMain from '../components/landing-page/FeaturesMain'
 
 export default function Home () {
   return (
     <>
       <Navbar />
       <Hero />
+      <FeaturesMain />
     </>
   )
 }
