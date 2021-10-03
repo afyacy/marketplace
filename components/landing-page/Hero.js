47ceaf50e2b948f1ec545eab33e29b64b7a62e65
Simba <joinsimba@gmail.com>
2021-10-03 18:01:35 +0200

    nextjs fixes

--- components/landing-page/Hero.js
+++ components/landing-page/Hero.js
@@ -1,23 +1,26 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
-      <div class="max-w-7xl mt-10 mx-auto block lg:flex px-7">
-        <div class="w-full lg:w-1/2">
-          <div class="font-semibold text-4xl md:text-6xl text-gray-900 mb-8">
-              <h2 class="max-w-[450px] text-5xl">
+      <div className="max-w-7xl mt-10 mx-auto block lg:flex px-7">
+        <div className="w-full lg:w-1/2">
+          <div className="font-semibold text-4xl md:text-6xl text-gray-900 mb-8">
+              <h2 className="max-w-[450px] text-5xl">
               All you need to manage and grow your local business in Africa.
               </h2>
           </div>
-          <p class="text-gray-500 mb-11 text-2xl font-light">
+          <p className="text-gray-500 mb-11 text-2xl font-light">
             Organize your business with automated online booking, reminders, payments and more—from one easy-to-use platform.
           </p>
-          <a class="py-3 px-4 bg-blue-800 text-gray-100 font-semibold rounded-lg shadow-md focus:outline-none cursor-pointer">
-            Request Access
-          </a>
+          <Link href="/register">
+            <a className="py-3 px-4 bg-blue-800 text-gray-100 font-semibold rounded-lg shadow-md focus:outline-none cursor-pointer">
+              Request Access
+            </a>
+          </Link>
         </div>
-        <div class="w-full lg:w-1/2 mt-8 lg:mt-0 lg:px-24">
+        <div className="w-full lg:w-1/2 mt-8 lg:mt-0 lg:px-24">
           <Image src='/images/header.png' alt="Header Image" width="443px" height="401px"/>
         </div>
       </div>
--- components/landing-page/Navbar.js
+++ components/landing-page/Navbar.js
@@ -1,15 +1,18 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
     <>
-      <nav class="flex items-center justify-between flex-wrap p-6">
+      <nav className="flex items-center justify-between flex-wrap p-6">
         <a>
           <Image src='/images/logo.svg' alt="Logo" className="h-8 w-10 mr-2" width="132px" height="36px"/>
         </a>
-        <a class="py-2 px-4 bg-blue-800 text-gray-100 font-semibold rounded-lg shadow-md focus:outline-none cursor-pointer">
-          Request Access
-        </a>
+        <Link href="/register">
+          <a className="py-2 px-4 bg-blue-800 text-gray-100 font-semibold rounded-lg shadow-md focus:outline-none cursor-pointer">
+            Request Access
+          </a>
+        </Link>
       </nav>
     </>
   )
