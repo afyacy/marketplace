668b26c9d391a07e9b05d4faae7c08073661ba56
Afyacy <25583917+afyacy@users.noreply.github.com>
2021-10-12 14:02:54 +0000

    nested writes in query, changed date type

--- pages/api/booking.js
+++ pages/api/booking.js
@@ -3,19 +3,17 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default async function handler (request, response) {
-  await prisma.attendee.create({
-    data: {
-      email: request.body.data.email,
-      name: request.body.data.name
-    }
-  })
-
   await prisma.event.create({
     data: {
-      startDate: request.body.data.startDate,
-      endDate: request.body.data.endDate,
+      startDate: new Date(Date.parse(request.body.data.startDate)),
+      endDate: new Date(Date.parse(request.body.data.endDate)),
       name: request.body.data.eventName,
-      user: request.body.data.companyEmail
+      user: request.body.data.companyEmail,
+      attendees: {
+        create: [
+          { email: request.body.data.email, name: request.body.data.name }
+        ]
+      }
     }
   })
   response.status(200).send('Success')
