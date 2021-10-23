7d91b12b6ced34ffd17f4d837179654188dd4f2b
Afyacy <25583917+afyacy@users.noreply.github.com>
2021-10-23 14:21:02 +0000

    redirect after login

---
+++ pages/booking/[email].js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import { useSession } from 'next-auth/client'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import router from 'next/router'
+import Navbar from '../../components/layout/Navbar'
+import { PrismaClient } from '@prisma/client'
+
+export default function Email ({ email, fullName }) {
+  const [session] = useSession()
+  const link = 'https://www.getmonto.com/'
+  const [value] = useState(`${link}booking/${email}`)
+  const [copySuccess, setCopySuccess] = useState('copy')
+
+  useEffect(() => {
+    if (!session) {
+      router.push('/login')
+    }
+  }, [])
+
+  return (
+    <div>
+      <Navbar/>
+      <div className="bg-teal-dark text-center">
+        <div className="text-white p-10">
+          <h1 className="p-4 md:text-4xl text-2xl">Welcome, {fullName}</h1>
+          <p>Copy event link and share with others to stay connected</p>
+        </div>
+        <div className="flex items-center justify-center w-screen">
+          <div className="rounded-full bg-white shadow flex w-4/12 p-2 justify-center">
+            <input
+              type="text"
+              defaultValue={value}
+              className="w-full rounded-tl-full rounded-bl-full py-2 px-6"
+            />
+            <button className="rounded-tr-full rounded-br-full py-2 px-4">
+              <p className="font-semibold text-base uppercase">
+                <CopyToClipboard text={value}
+                    onCopy={() => setCopySuccess('Copied')}>
+                    <span>{ copySuccess }</span>
+                </CopyToClipboard>
+              </p>
+            </button>
+          </div>
+        </div>
+
+        <button>See events</button>
+      </div>
+    </div>
+  )
+}
+
+export async function getServerSideProps ({ query }) {
+  const prisma = new PrismaClient()
+  const user = await prisma.user.findUnique({ where: query })
+  const email = user.email
+  const fullName = user.fullName
+  return {
+    props: { email, fullName }
+  }
+}
