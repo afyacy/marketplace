9043e05407e6561fcb2037f313b58e8b61bebb1f
Afyacy <25583917+afyacy@users.noreply.github.com>
2021-10-29 18:07:36 +0000

    fixed prisma issue in booking

---
+++ components/booking/event.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import DataTable from 'react-data-table-component'
+
+export default function Event ({ event }) {
+  return (
+    <div>
+      {event}
+    </div>
+  )
+}
--- pages/api/booking.js
+++ pages/api/booking.js
@@ -3,21 +3,19 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default async function handler (request, response) {
-  //  For Some reason the variables end up being undefined inside prisma instantiation
-  //  Kindly check if I am doing something uncalled for
-  //  const { name, email, title, location, startDate, endDate, userId } = request.body
+  const { name, email, title, location, startDate, endDate, userId } = request.body
   await prisma.event.create({
     data: {
-      title: 'Makeup',
-      location: 'Asokwa',
-      startDate: new Date('09/12/2021'),
-      endDate: new Date('09/12/2021'),
-      user: { connect: { id: 1 } },
+      title: title,
+      location: location,
+      startDate: new Date(startDate),
+      endDate: new Date(endDate),
+      user: { connect: { id: userId } },
       attendees: {
         create: [
           {
-            name: 'Roda',
-            email: 'roda@menubar.com'
+            name: name,
+            email: email
           }
         ]
       }
